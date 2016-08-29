eb62780bd8cfdc0c0d75708c01a2ffa8e9aea499
Kimmoonsu <Kimmoonsu@moonsuui-MacBook-Pro.local>
2016-08-29 18:57:02 +0900

    벽 부수기 update

--- src/no2206_1.java
+++ src/no2206_1.java
@@ -4,16 +4,16 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 
-public class no2206 {
-	int X;
-	int Y;
+
+public class no2206_1 {
+	int X, Y;
 	boolean map[][] = new boolean[1001][1001];
-	boolean visited[][][] = new boolean[1001][1001][3];
+	boolean visited[][][] = new boolean[1001][1001][2];
 	boolean isPass = false;
 	int short_cnt = 1;
+	
 	class Node {
-		int x;
-		int y;
+		int x, y;
 		int cnt;
 		Node(int x, int y, int cnt) {
 			this.x = x;
@@ -28,64 +28,57 @@ public class no2206 {
 		X = Integer.parseInt(str[0]);
 		Y = Integer.parseInt(str[1]);
 		for (int i = 1; i <= X; i++) {
-			String read_str = br.readLine();
-			for (int j = 0; j < read_str.length(); j++) {
-				if (read_str.charAt(j) == '0') map[i][j+1] = false;
+			String map_str = br.readLine();
+			for (int j = 0; j < map_str.length(); j++) {
+				if (map_str.charAt(j) == '0') map[i][j+1] = false;
 				else map[i][j+1] = true;
 			}
 		}
-		BFS_all(1, 1);
+		BFS_all(1,1);
 		if (isPass) System.out.println(short_cnt);
 		else System.out.println("-1");
-		
-//		for (int i = 1; i <= X; i++) {
-//			for (int j = 1; j <= Y; j++) {
-//				System.out.print(map[i][j]);
-//			}System.out.println();
-//		}
 	}
-	
 	void BFS_all(int x, int y) {
 		Queue<Node> queue = new LinkedList<Node>();
 		queue.offer(new Node(x, y, 1));
 		
-		while(!queue.isEmpty()) {
+		while (!queue.isEmpty()) {
 			int size = queue.size();
-			while (size-- > 0) {
+			while (size -- > 0) {
 				BFS(queue, queue.poll());
 				if (isPass) return;
 			}
-			short_cnt ++;
+			short_cnt++;
 		}
+		
 	}
-	
 	void BFS(Queue<Node> q, Node node) {
 		int dx[] = {1,-1,0,0};
 		int dy[] = {0,0,1,-1};
 		
-		if (node.x == X && node.y == Y) {
-			isPass = true;
-			return ;
-		}
 		int x = node.x;
 		int y = node.y;
 		int cnt = node.cnt;
-		for (int i = 0 ; i < 4; i ++) {
-			if (x + dx[i] > 0 && x + dx[i] <= X && y + dy[i] > 0 && y + dy[i] <= Y && visited[x + dx[i]][y + dy[i]][cnt] == false) {
+		if (x == X && y == Y) {
+			isPass = true;
+			return;
+		}
+		for (int i = 0; i < 4; i++) {
+			if (x+dx[i] > 0 && x+dx[i] <= X && y+dy[i] > 0 && y+dy[i] <= Y && visited[x+dx[i]][y+dy[i]][cnt] == false) {
 				if (map[x+dx[i]][y+dy[i]] == false) {
 					q.offer(new Node(x+dx[i], y+dy[i], cnt));
 					visited[x+dx[i]][y+dy[i]][cnt] = true;
-				} else {
-					if (cnt==1) {
+				}
+				else {
+					if (cnt == 1) {
 						q.offer(new Node(x+dx[i], y+dy[i], cnt-1));
-						visited[x+dx[i]][y+dy[i]][cnt-1] = true;						
+						visited[x+dx[i]][y+dy[i]][cnt-1] = true;
 					}
-
 				}
 			}
 		}
 	}
 	public static void main(String[] args) throws IOException {
-		new no2206().init();
+		new no2206_1().init();
 	}
 }
